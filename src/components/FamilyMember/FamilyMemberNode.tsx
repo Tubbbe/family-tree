976fe4a0b7a5fbeb16c65674b00a0e221fed2389
tubbbe <alexandre.monge33+github@gmail.com>
2025-07-29 21:33:09 +0200

    fix: handle position by mode

--- src/components/FamilyMember/FamilyMemberNode.tsx
+++ src/components/FamilyMember/FamilyMemberNode.tsx
@@ -1,15 +1,15 @@
-import {Handle, Position} from "@xyflow/react";
+import {Handle} from "@xyflow/react";
 import {FamilyMember, FamilyMemberBirthDeath, FamilyMemberContent, FamilyMemberName} from "./style";
 
 export function FamilyMemberNode(props: any) {
     return (
         <FamilyMember>
-            <Handle type="target" position={Position.Top} />
+            <Handle type="target" position={props.targetPosition} />
             <FamilyMemberContent>
                 <FamilyMemberName>{props.data.givenName + " " + props.data.surname.toUpperCase()}</FamilyMemberName>
                 <FamilyMemberBirthDeath>{props.data.born ? props.data.born : "????"}{props.data.death ? ` - ${props.data.death}` : ""}</FamilyMemberBirthDeath>
             </FamilyMemberContent>
-            <Handle type="source" position={Position.Bottom} />
+            <Handle type="source" position={props.sourcePosition} />
         </FamilyMember>
     );
 }
